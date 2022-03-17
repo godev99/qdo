6092009f32556d56b5bb741e4f08569e72b3f210
gab <givanes@pm.me>
2022-03-17 01:39:25 +0100

    first api

--- functions/dbInit.go
+++ functions/dbInit.go
@@ -1,11 +1,55 @@
 package example
 
 import (
+	"fmt"
+	"go.m3o.com/db"
 	"net/http"
 )
 
 func Helloworld(w http.ResponseWriter, r *http.Request) {
 	message := "Hello world!"
+
+	token := "MWYyOGFlMTEtZDk2Yy00YTliLTkzMDgtNzQyNzA1YzNmMjZk"
+
+	dbService := db.NewDbService(token)
+	_, err := dbService.Create(&db.CreateRequest{
+		Record: map[string]interface{}{
+			"Id":      "1",
+			"Title":   "Blue Train",
+			"Artiste": "John Coltrane",
+			"Price":   "56.99",
+		},
+		Table: "albums",
+	})
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	_, err = dbService.Create(&db.CreateRequest{
+		Record: map[string]interface{}{
+			"Id":      "2",
+			"Title":   "Jeru",
+			"Artiste": "Gerry Mulligan",
+			"Price":   "17.99",
+		},
+		Table: "albums",
+	})
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	_, err = dbService.Create(&db.CreateRequest{
+		Record: map[string]interface{}{
+			"Id":      "3",
+			"Title":   "Sarah Vaughan and Clifford Brown",
+			"Artiste": "Sarah Vaughan",
+			"Price":   "39.99",
+		},
+		Table: "albums",
+	})
+	if err != nil {
+		fmt.Println(err)
+	}
 
 	w.Write([]byte(message))
 }
